4674e7a375ef2cad070969bebae7b459f85e8834
LizNeira <lizneira@hotmail.com.ar>
2020-08-25 20:52:12 -0300

    se inicializo el proyecto(Controlador,EntradaYSalida y Main)

--- src/bodegon/EntradaYSalida.java
+++ src/bodegon/EntradaYSalida.java
@@ -1,6 +1,52 @@
 package bodegon;
 
+import static java.lang.Character.toUpperCase;
+import java.util.Scanner;
+
 public class EntradaYSalida
 {
-    
+   private static final Scanner entrada = new Scanner(System.in); 
+
+   private EntradaYSalida()
+   {
+   }
+
+   public static void mostrarMensaje(final String s)
+   {
+       System.out.print(s);
+   }
+
+   public static String leerCadena(String mensaje)
+   {
+       System.out.print(mensaje);
+       String entrada= new Scanner(System.in).nextLine();
+       return (entrada == null ? "" : entrada);
+   }
+   
+   public static int leerEntero(String mensaje)
+    {
+        System.out.print(mensaje);
+        return entrada.nextInt();
+    }   
+   
+   public static float leerDouble(String mensaje) 
+    {
+        System.out.print(mensaje);
+        return (float) entrada.nextDouble();
+    }
+    public static boolean leerBoolean(String texto)
+    {
+        System.out.println(texto);
+        String entrada = new Scanner(System.in).nextLine();
+
+        if (toUpperCase(entrada.charAt(0)) == 'S')
+        {
+            return true;
+        } 
+        else
+        {
+            return false;
+        }
+    }
+
 }
--- src/bodegon/Main.java
+++ src/bodegon/Main.java
@@ -4,6 +4,14 @@ package bodegon;
 public class Main {
     public static void main(String[] args)
     {
-        
+       Controlador controlador = new Controlador();
+        try
+        {
+            controlador.ejecutar();
+        } 
+        catch (NullPointerException e)
+        {
+            EntradaYSalida.mostrarMensaje(e.getMessage());
+        }
     }
 }
